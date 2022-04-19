5035d0f17bf931e1ed6922f82e04dc87881f43d4
Joe McIntyre <j_h_mcintyre@yahoo.com>
2022-04-19 16:12:45 -0500

    Upgrade to React 18, React-Redux 18, React types 18

--- app/client/src/components/app/index.tsx
+++ app/client/src/components/app/index.tsx
@@ -1,11 +1,15 @@
-import { render } from 'react-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from '../../store/store';
 import { App } from './App';
 
-render (
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById ('app')
+const container = document.getElementById ('app') as HTMLElement;
+const root = createRoot (container);
+root.render (
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 );
