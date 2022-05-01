fbad3b6b5d2a1c7f2d5bf2e906f57f3c0e7361d9
Joe McIntyre <j_h_mcintyre@yahoo.com>
2022-05-01 12:30:50 -0500

    client: center images narrower than card

--- app/client/src/components/pins/PinCard.tsx
+++ app/client/src/components/pins/PinCard.tsx
@@ -17,12 +17,16 @@ export const PinCard = ({
 
   return (
     <Paper>
-      <img
-        src={pin.url}
-        style={{ maxHeight: '140px', maxWidth: '100%', objectFit: 'cover' }}
-        alt='photo'
-        onError={(e) => { e.currentTarget.src = '/images/image404-75.png'; }}
-      />
+      <Box textAlign='center'>
+        <img
+          src={pin.url}
+          style={{ maxHeight: '140px', maxWidth: '100%', objectFit: 'cover' }}
+          alt='photo'
+          onError={(e) => {
+            e.currentTarget.src = '/images/image404-75.png';
+          }}
+        />
+      </Box>
       <Box p='4px'>
         <Typography textAlign='center' paragraph>{pin.title}</Typography>
         <Typography variant='body2'>{pin.text}</Typography>
